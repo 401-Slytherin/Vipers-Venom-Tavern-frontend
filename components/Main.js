05ea94a56462d563685c2bfa71e795c56541a70e
Anthony Lopez <Antl21292@gmail.com>
2023-08-19 19:44:55 -0400

    conditional rendering applied

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,20 @@
+
+import AboutUs from "@/pages/about";
+
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
-export default function Main() {
-  return(
+export default function Main({ seeLogin, seeSignup }) {
+  return (
     <main>
-      <LoginForm/>
-      <RegisterForm/>
+      {seeLogin || seeSignup ? (
+        <>
+          {seeLogin && <LoginForm />}
+          {seeSignup && <RegisterForm />}
+        </>
+      ) : (
+        <h1>Welcome to Viper's Venom Tavern!</h1>
+      )}
     </main>
   );
 }
