d6682da1e400fd7d2b68b869c9f2e3713f71cd68
Brenden Moore <brendenm3603@gmail.com>
2023-08-19 16:41:28 -0700

    Centered nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <nav className='bg-yellow-500 text-white p-4'>
-      <ul className='flex space-x-4'>
-        <li>
-          <a href='#' className='hover:text-yellow-100'>Buy</a>
-          <a href='#' className='hover:text-yellow-100'>Sell</a>
-          <a href='#' className='hover:text-yellow-100'>Trade</a>
-          <a href='#' className='hover:text-yellow-100'>About</a>
+    <nav className="bg-yellow-500 text-white p-4 flex justify-center">
+      <ul className="flex space-x-4 justify-between">
+        <li className="flex space-x-4">
+          <a href="#" className="hover:text-yellow-100">Buy</a>
+          <a href="#" className="hover:text-yellow-100">Sell</a>
+          <a href="#" className="hover:text-yellow-100">Trade</a>
         </li>
       </ul>
     </nav>
