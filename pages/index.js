c626289feacb67ca171b1ee120bbe6711c285fb7
Anthony Lopez <Antl21292@gmail.com>
2023-08-19 14:27:02 -0400

    small change

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 // import Image from 'next/image'
+import { useAuth } from '@/context/auth'
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Main from '@/components/Main'
 import Footer from '@/components/Footer'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { user, login, logout, register } = useAuth();
+
   return (
     <>
       <Head>
-        {/* <link rel='icon' href='../public/favicon.ico'/> */}
-        <title>Viper's Venom </title>
+        <link rel='icon' href='../public/favicon.ico'/>
+        <title>Viper's Venom</title>
       </Head>
       <Header/>
       <Main/>
