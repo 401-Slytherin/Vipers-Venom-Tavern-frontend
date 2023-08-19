89501a73222062f3c8070fee246f13f7dd4b1230
Anthony Lopez <Antl21292@gmail.com>
2023-08-19 19:44:29 -0400

    connected signup and login buttons

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,61 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
+import ActiveLink from "@/hooks/router";
+import LoginForm from "./LoginForm";
 
-export default function Header() {
+export default function Header({
+  handleLogin,
+  handleSignup,
+  seeLogin,
+  seeSignup,
+}) {
   return (
     <header className="bg-cover bg-center bg-wood-pattern text-white py-20 flex justify-between items-center">
       <div className="text-center">
-        <Image className="pl-10"
+        <Image
+          className="pl-10"
           src="/cobra.png"
           alt="Vipers venom logo"
           objectFit="cover"
           width={500}
           height={100}
         />
-        <h1 className="text-4xl pl-20 font-bold font-sans">Viper's Venom Tavern</h1>
+        <h1 className="text-4xl pl-20 font-bold font-sans">
+          Viper's Venom Tavern
+        </h1>
         <p className="text-lg"></p>
       </div>
       <div className="space-x-4 pr-10">
-        <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded">
-          Sign Up
-        </button>
-        <button className="bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded">
-          Log In
-        </button>
+        {!seeLogin && !seeSignup ? (
+          <>
+            <button
+              onClick={handleSignup}
+              className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            >
+              Sign Up
+            </button>
+            <button
+              onClick={handleLogin}
+              className="bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded"
+            >
+              Log In
+            </button>
+          </>
+        ) : seeLogin ? (
+          <button
+            onClick={handleSignup}
+            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+          >
+            Sign Up
+          </button>
+        ) : (
+          <button
+            onClick={handleLogin}
+            className="bg-gray-700 hover:bg-gray-800 text-white px-4 py-2 rounded"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </header>
   );
