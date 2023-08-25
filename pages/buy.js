1bf7e4795e6ed22a4164a4fed4c199686131c381
Araceligm90 <garcia.araceli90@gmail.com>
2023-08-25 03:46:44 +0200

    profile page working

--- pages/buy.js
+++ pages/buy.js
@@ -1,6 +1,5 @@
 // import Image from 'next/image';
 import React from "react";
-import { useAuth } from '@/context/auth';
 import { useRouter } from 'next/router';
 import NavBar2 from "@/components/NavBar2"
 import Header from "@/components/Header";
@@ -23,103 +22,6 @@ export default function BuyPage() {
     <>
       <Header user={user} logout={logout} />
       <NavBar2 />
-    
-      {/* <div className="flex flex-wrap justify-center">
-        First Card
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
-          </a>
-          <a href="#">
-            <Image
-              src="/card_image"
-              alt="Card's image"
-              objectFit="card"
-              width={40}
-              height={40}
-            />
-          </a>
-          <div className="p-5">
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card description goes here.</p>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card price goes here.</p>
-            <button
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg focus:ring focus:outline-none"
-              style={{ backgroundColor: 'red', color: 'white' }}
-              onClick={(e) => {
-                const button = e.target;
-                button.style.backgroundColor = button.style.backgroundColor === 'red' ? 'green' : 'red';
-              }}
-            >
-              Wishlist
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-center">
-        Second Card
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
-          </a>
-          <a href="#">
-            <Image
-              src="/card_image"
-              alt="Card's image"
-              objectFit="card"
-              width={40}
-              height={40}
-            />
-          </a>
-          <div className="p-5">
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card description goes here.</p>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card price goes here.</p>
-            <button
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg focus:ring focus:outline-none"
-              style={{ backgroundColor: 'red', color: 'white' }}
-              onClick={(e) => {
-                const button = e.target;
-                button.style.backgroundColor = button.style.backgroundColor === 'red' ? 'green' : 'red';
-              }}
-            >
-              Wishlist
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-wrap justify-center">
-        Third Card
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
-          </a>
-          <a href="#">
-            <Image
-              src="/card_image"
-              alt="Card's image"
-              objectFit="card"
-              width={40}
-              height={40}
-            />
-          </a>
-          <div className="p-5">
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card description goes here.</p>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Card price goes here.</p>
-            <button
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg focus:ring focus:outline-none"
-              style={{ backgroundColor: 'red', color: 'white' }}
-              onClick={(e) => {
-                const button = e.target;
-                button.style.backgroundColor = button.style.backgroundColor === 'red' ? 'green' : 'red';
-              }}
-            >
-              Wishlist
-            </button>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div> */}
-
       <Main2 cards={resources || []} />
       <Footer user={user}/>  
     </>
