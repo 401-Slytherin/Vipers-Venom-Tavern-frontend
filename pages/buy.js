e2ea642355a8db533f5c24c35baca19b2966daa8
Anthony Lopez <Antl21292@gmail.com>
2023-08-24 21:07:30 -0400

    commented out

--- pages/buy.js
+++ pages/buy.js
@@ -17,8 +17,8 @@ export default function BuyPage() {
       <Header />
       <NavBar2 />
     
-      <div className="flex flex-wrap justify-center">
-        {/* First Card */}
+      {/* <div className="flex flex-wrap justify-center">
+        First Card
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
@@ -49,7 +49,7 @@ export default function BuyPage() {
         </div>
       </div>
       <div className="flex flex-wrap justify-center">
-        {/* Second Card */}
+        Second Card
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
@@ -76,17 +76,17 @@ export default function BuyPage() {
             >
               Wishlist
             </button>
-            {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-            </a> */}
+            </a>
           </div>
         </div>
       </div>
       <div className="flex flex-wrap justify-center">
-        {/* Third Card */}
+        Third Card
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-4">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Card title goes here</h5>
@@ -113,20 +113,19 @@ export default function BuyPage() {
             >
               Wishlist
             </button>
-            {/* <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-            </a> */}
+            </a>
           </div>
         </div>
       </div>
       <Footer />
-    </div>
+    </div> */}
 
       <Main2 cards={resources || []} />
-      <p>Placeholder</p>
       <Footer />  
     </>
   );
