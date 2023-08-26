bff98718d8fac7078be7894bcd41cf4fb305f4d1
Brenden Moore <brendenm3603@gmail.com>
2023-08-26 09:22:56 -0700

    added back useauth

--- pages/buy.js
+++ pages/buy.js
@@ -1,5 +1,6 @@
 // import Image from 'next/image';
 import React from "react";
+import { useAuth } from "@/context/auth";
 import { useRouter } from 'next/router';
 import NavBar from "@/components/NavBar"
 import Header from "@/components/Header";
